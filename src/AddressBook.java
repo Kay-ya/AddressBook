3ea11de015e3a8f886750b9d8cb949bfa4240dcd
keyapatel <keyapatel4@cmail.carleton.ca>
2022-09-19 07:37:28 -0400

    Added main method to class

---
+++ src/AddressBook.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class AddressBook {
+
+    private HashMap<String, String> BudInfo;
+
+    public AddressBook(){
+        BudInfo = new HashMap<>();
+    }
+
+    public void addBuddy() {
+    }
+
+    public void removeBuddy() {
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+
+}
+
