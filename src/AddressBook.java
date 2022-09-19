1e88e5c0f38ecd41784076b96413641e64c9336b
keyapatel <keyapatel4@cmail.carleton.ca>
2022-09-19 15:28:55 -0400

    Changed main method to add and remove buddy from address book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -8,14 +8,14 @@ public class AddressBook {
         BudInfo = new HashMap<>();
     }
 
-    public void addBuddy() {
+    public void addBuddy(BuddyInfo bud) {
     }
 
-    public void removeBuddy() {
+    public void removeBuddy(BuddyInfo bud) {
     }
 
     public static void main(String[] args){
-        BuddyInfo buddy = new BuddyInfo("TOM", "Carleton", "123");
+        BuddyInfo buddy = new BuddyInfo("TOM", "Carleton", 123);
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
