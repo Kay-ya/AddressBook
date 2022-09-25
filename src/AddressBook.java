38912bf0092713379d9e78a906b4e2430f188e38
keyapatel <keyapatel4@cmail.carleton.ca>
2022-09-24 22:15:17 -0400

    Changed main method to add and remove buddy from address book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,26 +1,34 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class AddressBook {
 
-    private HashMap<String, String> BudInfo;
+    private ArrayList<BuddyInfo> infobud;
 
     public AddressBook(){
-        BudInfo = new HashMap<>();
+        infobud = new ArrayList<>();
     }
 
     public void addBuddy(BuddyInfo bud) {
+        if (bud != null){
+            infobud.add(bud);
+        }
     }
 
-    public void removeBuddy(BuddyInfo bud) {
+    public BuddyInfo removeBuddy(int index) {
+        if (index >=0 && index < infobud.size()){
+            return infobud.remove(index);
+        }
+        return null;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("TOM", "Carleton", 123);
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
-        //System.out.println("Address1 Book");
+        addressBook.removeBuddy(0);
     }
 
 }
 
+
+//https://github.com/Kay-ya/AddressBook.git
