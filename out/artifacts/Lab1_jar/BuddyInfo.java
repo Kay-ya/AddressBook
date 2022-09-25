0b61a9cba54da57cc87fde2931d5a31ac03d1a86
keyapatel <keyapatel4@cmail.carleton.ca>
2022-09-25 09:42:44 -0400

    Changed main method to add and remove buddy from address book

---
+++ out/artifacts/Lab1_jar/BuddyInfo.java
@@ -0,0 +1,30 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private int number;
+
+    public BuddyInfo(String name, String address, int number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public static void main(String[] args) {
+
+        BuddyInfo buddy1 = new BuddyInfo("Henry", "63 Pivet Drive, London, UK", 1234567890);
+        System.out.println("Hello " + buddy1.getName());
+    }
+}
